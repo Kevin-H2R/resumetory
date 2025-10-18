fb478e4638a8e938b8e0ede073d3f467151146f9
Kevin <herrbach.kevin@gmail.com>
2025-10-18 23:42:15 +0900

    ADds more user info migration

---
+++ prisma/migrations/20251018144128_adds_user_basic_info/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "firstname" TEXT,
+ADD COLUMN     "lastname" TEXT,
+ADD COLUMN     "link" TEXT,
+ADD COLUMN     "linkedIn" TEXT,
+ADD COLUMN     "location" TEXT,
+ADD COLUMN     "phoneCode" TEXT,
+ADD COLUMN     "phoneNumber" TEXT;
